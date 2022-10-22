cf661128a4452bcfcb99788ebb71150dbd560516
EC2 Default User <ec2-user@ip-172-31-26-42.ap-northeast-1.compute.internal>
2022-10-22 08:41:39 +0000

    [add]error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,30 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
 
   def after_sign_in_path_for(resource)
-    root_path
+    if current_user
+      flash[:notice] = "Signed successfully."
+      user_path(current_user.id)
+    else
+      new_user_registration_path
+    end
+  end
+
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "Welcome! You have signed up successfully."
+    user_path(current_user.id)
   end
 
   def after_sign_out_path_for(resource)
+    flash[:notice] = "Signed out successfully."
     root_path
   end
 
+  protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -8,10 +8,13 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user = current_user
     @users = User.all
     @book = Book.new
+  end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
