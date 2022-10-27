2d52a7cfea803fe2fbf9a8cead9ed97fe670a9b9
EC2 Default User <ec2-user@ip-172-31-26-42.ap-northeast-1.compute.internal>
2022-10-27 07:34:21 +0000

    [add]いいね機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-
+  has_many :favorites, dependent: :destroy
+  has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-  has_one_attached :profile_image
-  
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
